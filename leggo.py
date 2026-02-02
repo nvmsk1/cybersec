1c00d139c6af15c9c11dc6a08dd61ebd86c1da36
nvmsk1 <hikeri1212@gmail.com>
2026-02-02 10:07:26 +0100

    Update leggo.py

--- leggo.py
+++ leggo.py
@@ -1,1 +1,7 @@
-#non ricordo nulla
+namefile = logfile.txt
+try: 
+    with open(namefile, "r", encoding="utf-8") as f:
+        for riga in f:
+            print(riga, end="")
+except FileNotFoundError as e:
+    print(f"[!] errore: {e}")
