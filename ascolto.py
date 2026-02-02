6d20bf80e341d1b35ccd6a6063894bb81c272783
nvmsk1 <hikeri1212@gmail.com>
2026-02-02 11:13:05 +0100

    Update ascolto.py

--- ascolto.py
+++ ascolto.py
@@ -1,1 +1,32 @@
+import socket
 
+HOST = '127.0.0.1'
+PORT = 6364
+BUFFER_SIZE = 1024
+
+server = socket.socket()
+server.bind((HOST, PORT))
+server.listen (1)
+
+print ("in ascolto su {HOST}:{PORT}")
+
+for _ in range(6):
+    conn, addr = server.accept()
+    print("connesso")
+
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+
+        richiesta = data.decode()
+
+        if richiesta == "SHUTDOWN":
+            break
+
+        risposta = "ricevuto il messaggio {richiesta}"
+        conn.sendall(risposta.encode())
+
+    conn.close()
+
+server.close()   
